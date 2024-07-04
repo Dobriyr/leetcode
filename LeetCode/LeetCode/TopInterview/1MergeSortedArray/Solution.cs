c7f95e6f4b47bff20b5221fff079b65a5b242910
Roman Kuzmych <roman.kuzmich@gmail.com>
2024-07-04 15:08:23 +0300

    2024-07-04

---
+++ LeetCode/LeetCode/TopInterview/1MergeSortedArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.TopInterview._1MergeSortedArray
+{
+    /// <summary>
+    /// Easy - Merge Sorted Array
+    /// </summary>
+    public static class Solution
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m + n - 1;
+            int pointer1 = m - 1;
+            int pointer2 = n - 1;
+
+            while (pointer2 >= 0)
+            {
+                if (pointer1 >= 0 && nums1[pointer1] > nums2[pointer2])
+                {
+                    nums1[i] = nums1[pointer1--];
+                }
+                else
+                {
+                    nums1[i] = nums2[pointer2--];
+                }
+                i--;
+            }
+        }
+    }
+}
