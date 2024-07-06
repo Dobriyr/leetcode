ddc42c7780dbfef8bb219c1ba9f556d462957e56
Roman Kuzmych <roman.kuzmich@gmail.com>
2024-07-06 21:53:34 +0300

    2024-07-06

---
+++ LeetCode/LeetCode/Y2024/M07/D06/Implementation2024-07-06.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Y2024.M06.D06
+{
+    /// <summary>
+    /// Pass the Pillow
+    /// </summary>
+    public static class Implementation2024_07_06
+    {
+        public static int PassThePillow(int n, int time)
+        {
+            int passCount = time / (n - 1);
+            return passCount % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+        }
+    }
+}
