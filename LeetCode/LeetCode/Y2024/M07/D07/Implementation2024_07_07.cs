4e754769a4441c1fac858f1370eb650322bb3ba3
Roman Kuzmych <roman.kuzmich@gmail.com>
2024-07-07 22:00:09 +0300

    2024-07-07

---
+++ LeetCode/LeetCode/Y2024/M07/D07/Implementation2024_07_07.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Y2024.M07.D07
+{
+    /// <summary>
+    /// Water Bottles
+    /// </summary>
+    public static class Implementation2024_07_07
+    {
+        public static class Solution
+        {
+            public static int NumWaterBottles(int numBottles, int numExchange)
+            {
+                var sum = 0;
+                var empty = 0;
+
+                while (numBottles > 0)
+                {
+                    sum += numBottles;
+                    empty += numBottles;
+                    numBottles = empty / numExchange;
+                    empty = empty - numBottles * numExchange;
+                }
+
+                return sum;
+            }
+        }
+    }
+}
