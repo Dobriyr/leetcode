ca8e82c6b069c7bfd08f347932ab98b8f69d94ff
Roman Kuzmych <rkuzmy@softserveinc.com>
2024-07-10 15:58:11 +0300

    2024-07-10

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 
 
 TestCase(1);
-Console.WriteLine(Implementation2024_07_00.AverageWaitingTime([[1, 2], [2, 5], [4, 3]]));
+Console.WriteLine(Implementation2024_07_09.AverageWaitingTime([[1, 2], [2, 5], [4, 3]]));
 
 TestCase(2);
-Console.WriteLine(Implementation2024_07_00.AverageWaitingTime([[5, 2], [5, 4], [10, 3], [20, 1]]));
+Console.WriteLine(Implementation2024_07_09.AverageWaitingTime([[5, 2], [5, 4], [10, 3], [20, 1]]));
 
 static void TestCase(int number)
 {
---
+++ LeetCode/LeetCode/Y2024/M07/D10/Implementation2024_07_10.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Y2024.M07.D10
+{
+    /// <summary>
+    /// Crawler Log Folder
+    /// </summary>
+    public static class Implementation2024_07_10
+    {
+        private const string stepback = "../";
+
+        public static int MinOperations(string[] logs)
+        {
+            int curentDepth = 0;
+
+            foreach (var log in logs)
+            {
+                if (log == stepback)
+                {
+                    curentDepth--;
+
+                    if (curentDepth < 0)
+                        curentDepth = 0;
+                }
+                if (log[0] != '.')
+                {
+                    curentDepth++;
+                }
+            }
+
+            return curentDepth;
+        }
+
+    }
+}
