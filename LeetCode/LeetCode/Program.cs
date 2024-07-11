5ef0e6e127fb7e8519d8b3a09f9a26038e17d337
Roman Kuzmych <rkuzmy@softserveinc.com>
2024-07-11 14:02:18 +0300

    2024-07-03

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -2,14 +2,18 @@
 using LeetCode.Y2024.M07.D04;
 using LeetCode.Y2024.M07.D08;
 using LeetCode.Y2024.M07.D09;
+using LeetCode.Y2024.M07.D11;
 using System.Runtime.InteropServices;
 
 
 TestCase(1);
-Console.WriteLine(Implementation2024_07_09.AverageWaitingTime([[1, 2], [2, 5], [4, 3]]));
+Console.WriteLine(Implementation2024_07_11.ReverseParentheses("(abcd)"));
 
 TestCase(2);
-Console.WriteLine(Implementation2024_07_09.AverageWaitingTime([[5, 2], [5, 4], [10, 3], [20, 1]]));
+Console.WriteLine(Implementation2024_07_11.ReverseParentheses("(u(love)i)"));
+
+TestCase(2);
+Console.WriteLine(Implementation2024_07_11.ReverseParentheses("(ed(et(oc))el)"));
 
 static void TestCase(int number)
 {
---
+++ LeetCode/LeetCode/Y2024/M07/D11/Implementation2024_07_11.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LeetCode.Y2024.M07.D11
+{
+    /// <summary>
+    /// Reverse Substrings Between Each Pair of Parentheses
+    /// </summary>
+    public static class Implementation2024_07_11
+    {
+        public static string ReverseParentheses(string s)
+        {
+            var arr = s.ToCharArray();
+
+            Stack<int> positions = new Stack<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == '(')
+                {
+                    positions.Push(i);
+                }
+                else if (arr[i] == ')')
+                {
+                    int j = positions.Pop();
+                    Reverce(arr, j, i);
+
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (var a in arr)
+            {
+                if (a != '(' && a != ')')
+                {
+                    result.Append(a);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static void Reverce(char[] array, int start, int end)
+        {
+
+            char cup;
+
+            while (start < end)
+            {
+                cup = array[start];
+                array[start] = array[end];
+                array[end] = cup;
+                start++;
+                end--;
+            }
+       }
+    }
+}
